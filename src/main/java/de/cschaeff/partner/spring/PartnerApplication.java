607a6019f65b823c9d7913cc169bf50f1ecf5481
claus <claus@test.de>
2021-10-17 12:55:07 +0200

    Add pom

---
+++ src/main/java/de/cschaeff/partner/spring/PartnerApplication.java
@@ -0,0 +1,11 @@
+package de.cschaeff.partner.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartnerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PartnerApplication.class);
+    }
+}
